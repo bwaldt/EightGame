dbcde2dec1f466e2de000f715dd27546d20a58c1
bwaldt <jbrooke@seas.upenn.edu>
2017-05-19 07:20:31 -0400

    intial setups

--- homework3.py
+++ homework3.py
@@ -8,7 +8,7 @@ student_name = "Type your full name here."
 # Imports
 ############################################################
 
-# Include your imports here, if any are used.
+import random
 
 
 
@@ -17,31 +17,70 @@ student_name = "Type your full name here."
 ############################################################
 
 def create_tile_puzzle(rows, cols):
-    pass
+    x = [ (i + 1) % (rows*cols) for i in range(rows * cols)]
+    s = [x[i:i + cols] for i in range(0, len(x), cols)]
+    return TilePuzzle(s)
 
 class TilePuzzle(object):
     
     # Required
     def __init__(self, board):
         self.board = board
+        self.rows = len(board)
+        self.cols = len(board[0])
+        self.zero = [(i, board.index(0)) for i, board in enumerate(board) if 0 in board]
+        self.zero_row = self.zero[0][0]
+        self.zero_col = self.zero[0][1]
 
     def get_board(self):
-        pass
+        return self.board
 
     def perform_move(self, direction):
-        pass
+        col = self.zero_col
+        row = self.zero_row
+        if direction == "left" and self.zero_col != 0:
+            self.board[row][col-1], self.board[row][col] = self.board[row][col], self.board[row][col-1]
+            self.zero_col -= 1
+            return True
+        if direction == "right" and self.zero_col != self.cols-1:
+            self.board[row][col+1], self.board[row][col] = self.board[row][col], self.board[row][col+1]
+            self.zero_col += 1
+            return True
+        if direction == "up" and self.zero_row != 0:
+            self.board[row-1][col], self.board[row][col] = self.board[row][col], self.board[row-1][col]
+            self.zero_row -= 1
+            return True
+        if direction == "down" and self.zero_row != self.rows-1:
+            self.board[row+1][col], self.board[row][col] = self.board[row][col], self.board[row+1][col]
+            self.zero_row += 1
+            return True
+        else:
+            return False
 
     def scramble(self, num_moves):
-        pass
-
+        input = ["up", "down", "right", "left"]
+        moves = 0
+        while moves < num_moves:
+            if self.perform_move(random.choice(input)) == True:
+                moves += 1
+        print moves
     def is_solved(self):
-        pass
+        test = create_tile_puzzle(self.rows,self.cols)
+        print self.get_board() == test.get_board()
+
+
 
     def copy(self):
-        pass
+        return self
 
     def successors(self):
-        pass
+        input = ["up", "down", "right", "left"]
+        ans = ()
+        for y in input:
+            p2 = self.copy()
+            p2.perform_move(y)
+            ans = (y , p2.get_board() )
+            print ans
 
     # Required
     def find_solutions_iddfs(self):
@@ -51,82 +90,7 @@ class TilePuzzle(object):
     def find_solution_a_star(self):
         pass
 
-############################################################
-# Section 2: Grid Navigation
-############################################################
 
-def find_path(start, goal, scene):
-    pass
+p = create_tile_puzzle(3, 3)
+p.successors()
 
-############################################################
-# Section 3: Linear Disk Movement, Revisited
-############################################################
-
-def solve_distinct_disks(length, n):
-    pass
-
-############################################################
-# Section 4: Dominoes Game
-############################################################
-
-def create_dominoes_game(rows, cols):
-    pass
-
-class DominoesGame(object):
-
-    # Required
-    def __init__(self, board):
-        pass
-
-    def get_board(self):
-        pass
-
-    def reset(self):
-        pass
-
-    def is_legal_move(self, row, col, vertical):
-        pass
-
-    def legal_moves(self, vertical):
-        pass
-
-    def perform_move(self, row, col, vertical):
-        pass
-
-    def game_over(self, vertical):
-        pass
-
-    def copy(self):
-        pass
-
-    def successors(self, vertical):
-        pass
-
-    def get_random_move(self, vertical):
-        pass
-
-    # Required
-    def get_best_move(self, vertical, limit):
-        pass
-
-############################################################
-# Section 5: Feedback
-############################################################
-
-feedback_question_1 = """
-Type your response here.
-Your response may span multiple lines.
-Do not include these instructions in your response.
-"""
-
-feedback_question_2 = """
-Type your response here.
-Your response may span multiple lines.
-Do not include these instructions in your response.
-"""
-
-feedback_question_3 = """
-Type your response here.
-Your response may span multiple lines.
-Do not include these instructions in your response.
-"""
